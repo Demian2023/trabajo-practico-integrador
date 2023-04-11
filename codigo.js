7c7e2b70dcc5f6f7c7dba6a7df94f5ceda3ea242
Demian2023 <116525747+Demian2023@users.noreply.github.com>
2023-04-11 14:03:11 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -9,30 +9,29 @@ $(document).ready(function() {
 });
 
 $("#formulario").validate({
-    rules: {
-        nombre: {
-            required: true,
-            minlength: 3,
-            maxlength: 30,
-        },
-        apellido: {
-            required: true,
-            minlength: 3,
-            maxlength: 30,
-        },
-        email: {
-            required: true,
-            email: true,
-        },
-        messages: {
+  rules: {
+    nombre: {
+      required: true,
+      minlength: 3,
+      maxlength: 30
+    },
+    apellido: {
+      required: true,
+      minlength: 3,
+      maxlength: 30
+    },
+    email: {
+      required: true,
+      email: true
+    }
+  },
+  messages: {
     'nombre.required': 'Por favor, introduce tu nombre',
     'apellido.required': 'Por favor, introduce tu apellido',
     'email.required': 'Por favor, introduce tu dirección de correo electrónico',
     'email.email': 'Por favor, introduce una dirección de correo electrónico válida'
   }
-}
 });
-
 
 
 function validar(){
