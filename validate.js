ddd2f1ea6cdd86f6787233cd4694f5c5f17ac71a
Demian2023 <116525747+Demian2023@users.noreply.github.com>
2023-04-11 14:34:25 -0300

    Create validate.js

---
+++ validate.js
@@ -0,0 +1,31 @@
+
+$("#formulario").validate({
+  rules: {
+    nombre: {
+      required: true,
+      minlength: 3,
+      maxlength: 30
+    },
+    apellido: {
+      required: true,
+      minlength: 3,
+      maxlength: 30
+    },
+    email: {
+      required: true,
+      email: true
+    }
+  },
+  messages: {
+    'nombre.required': 'Por favor, introduce tu nombre',
+    'apellido.required': 'Por favor, introduce tu apellido',
+    'email.required': 'Por favor, introduce tu dirección de correo electrónico',
+    'email.email': 'Por favor, introduce una dirección de correo electrónico válida'
+  }
+});
+
+
+function validar(){
+    if($("#formulario").valid() == false){
+        return;
+    }
